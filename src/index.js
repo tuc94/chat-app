e4fc86ca90db3d5822a10d41e4f306174d0bb193
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-02-23 21:16:48 -0500

    added send geolocation button and functinality

--- src/index.js
+++ src/index.js
@@ -12,8 +12,26 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
-  console.log("New Websocket connection");
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection!");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
+
+  socket.on("sendlocation", (location) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+  });
 });
 
 server.listen(port, () => {
